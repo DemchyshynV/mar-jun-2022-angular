e70c6f26ef9d2d78b0705e4c38a71f0ad15882b6
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2022-10-24 20:58:05 +0300

    master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UsersComponent } from './components/users/users.component';
-import { UserComponent } from './components/user/user.component';
 import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
-    UserComponent
   ],
   imports: [
     BrowserModule,
--- src/app/interfaces/user.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface IUser {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-}
